11aa27c98eab6ef2c2c0576b7317220bcd8e0ce2
Tristan Farmer <tristanfarmer@gmail.com>
2017-11-27 07:28:38 -0500

    Created Hardcoded Pantry List Pulled onSubmit from UPCScanner.js into Pantry.js

--- src/Pantry.js
+++ src/Pantry.js
@@ -2,11 +2,106 @@ import React from 'react';
 import UPCScanner from './UPCScanner';
 
 export default class Pantry extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pantryItems:
+        [{
+          name: '1 Gallon 2% Milk',
+          description: '1 Gallon of 2% Fat Dairy Milk.',
+          quantity: '1',
+        },
+        {
+          name: '12 Pack Jumbo Eggs',
+          description: '12 Pack Jumbo Free Range Chicken Eggs.',
+          quantity: '1',
+        },
+        {
+          name: 'Oreo Double Stuff',
+          description: 'Crunchy chocolate cookies with a sweet cream in the middle.',
+          quantity: '2',
+        }],
+    };
+    this.onUPCSubmit = this.onUPCSubmit.bind(this);
+  }
+
+  onUPCSubmit(event, item) {
+    event.preventDefault();
+    this.setState({
+      item: item,
+    });
+  }
+
   render() {
+    let description = null;
+    if (this.state.item && !this.state.item.name) {
+      description = <h3>Product Not Found.</h3>;
+    }
+    else {
+      description = <ProductDescription item={this.state.item} />
+    }
+
     return (
-      <div>
-        <UPCScanner />
+      <div className='container'>
+        <div className='row'>
+          <div className='col'>
+            <ul className='list-group'>
+              <li className='list-group-item'><UPCScanner onSubmit={this.onUPCSubmit} /></li>
+              <li className='list-group-item'>{description}</li>
+            </ul>
+          </div>
+          <div className='col'>
+            <Inventory pantryItems={this.state.pantryItems}/>
+          </div>
+        </div>
       </div>
     )
   }
+}
+
+function Inventory(props) {
+  return (
+    <div>
+
+      <ul className='list-group'>
+        <li className='list-group-item'><h2>Your Pantry</h2></li>
+        {props.pantryItems.map((item) => {
+          return <InventoryItem key={item.name} item={item}/>
+        })}
+      </ul>
+    </div>
+  )
+}
+
+function InventoryItem(props) {
+  return (
+    <li className='list-group-item'>
+      <dl className='dl-horizontal'>
+        <dt>Name</dt><dd>{props.item.name}</dd>
+        <dt>Description</dt><dd>{props.item.description}</dd>
+        <dt>Quantity</dt><dd>{props.item.quantity}</dd>
+      </dl>
+    </li>
+  )
+}
+
+/*
+ * Product Description Component
+ */
+function ProductDescription(props) {
+  if (!props.item) {
+    return null;
+  }
+
+  return (
+    <div>
+      <h2>Product Description</h2>
+      <dl className='dl-horizontal'>
+        <dt>SKU#</dt><dd>{props.item.upc}</dd>
+        <dt>Name</dt><dd>{props.item.name}</dd>
+        <dt>Description</dt><dd>{props.item.description}</dd>
+        <dt>Quantity</dt><dd>{props.item.quantity}</dd>
+      </dl>
+    </div>
+  )
 }
--- src/UPCScanner.js
+++ src/UPCScanner.js
@@ -7,70 +7,37 @@ export default class UPCScanner extends React.Component {
       upcInput: '',
     }
     this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   // Store input changes
   handleChange(event) {
     this.setState({
       upcInput: event.target.value,
-    });
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-    this.setState({
-      // TODO This will be a lookup. Leave item empty if not found.
-      item: {
-        upc: this.state.upcInput,
-        name: 'Nilla Wafers',
-        description: 'Soft and sweet wafers. Great for Banana Pudding.',
-        quantity: '2',
-      },
+      item: lookUpUPC(event.target.value),
     });
   }
 
   render() {
     return (
-      <div className='container'>
-        <div className='row'>
-          <div className='col'>
-            <form onSubmit={this.handleSubmit}>
-              <div className='form-group'>
-                <label htmlFor='upcInput'><h2>Scan or Enter UPC Here</h2></label>
-                  <input type='text' id='upcInput' className='form-control' value={this.state.upcInput}
-                    onChange={this.handleChange} onBlur={this.handleSubmit}/>
-              </div>
-            </form>
-          </div>
+      <form onSubmit={(e) => this.props.onSubmit(e, this.state.item)}>
+        <div className='form-group'>
+          <label htmlFor='upcInput'><h2>Scan or Enter UPC Here</h2></label>
+            <input type='text' id='upcInput' className='form-control' value={this.state.upcInput}
+              onChange={this.handleChange} onBlur={(e) => this.props.onSubmit(e, this.state.item)}/>
         </div>
-        <div className='row'>
-          <div className='col'>
-            <ProductDescription item={this.state.item} />
-          </div>
-        </div>
-      </div>
+      </form>
     )
   }
 }
 
-/*
- * Product Description Component
- */
-function ProductDescription(props) {
-  if (!props.item) {
-    return null;
+function lookUpUPC(upc) {
+  if (upc === '1111') {
+    return {
+      upc: upc,
+      name: 'Nilla Wafers',
+      description: 'Soft and sweet wafers. Great for Banana Pudding.',
+      quantity: '2',
+    };
   }
-
-  return (
-    <div>
-      <h2>Product Description</h2>
-      <dl className='dl-horizontal'>
-        <dt>SKU#</dt><dd>{props.item.upc}</dd>
-        <dt>Name</dt><dd>{props.item.name}</dd>
-        <dt>Description</dt><dd>{props.item.description}</dd>
-        <dt>Quantity</dt><dd>{props.item.quantity}</dd>
-      </dl>
-    </div>
-  )
+  return upc === '' ? null : {};
 }
